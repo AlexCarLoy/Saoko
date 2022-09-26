baa52bc8da1a2733f3bd10684afcc857d2ad29e5
Alejandro Cardona <dalejndro.cl.1910@gmail.com>
2022-09-26 10:03:14 -0600

    feat: cambio del script

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Saoko papi saoko");
+document.getElementById("Generation").style.color = "grey";
+function colorTitulo(){
+    console.log("Pulso el boton de Generation");
+    document.getElementById("Generation").style.color = "red";
+}
